c56bd4d1a953808651bd5d7e3a0c2a3516bf8255
Fernando Varela Morales <vrlm.fernando@gmail.com>
2023-05-04 16:25:05 -0600

    CSS optimization

--- script.js
+++ script.js
@@ -1,89 +1,86 @@
 "use strict";
-const components = selector => document.querySelector(selector);
-// Containers
-const initialContent = components(".container-two__initial-content");
-const submittedTextContainer = components(".container-two__header");
-// Textareas
-const typeText = components(".header__type-text");
-const submittedText = components(".header__submitted-text");
-// Buttons
-const buttonEncrypt = components(".button-encrypt");
-const buttonDecrypt = components(".button-decrypt");
-const buttonCopy = components(".button-copy");
+const components = (selector) => document.querySelector(selector);
+
+const notContent = components(".et-container.notMsg");
+const content = components(".et-container.msg");
+const typeText = components(".et-text.type");
+const showText = components(".et-text.show");
+const buttonEncrypt = components(".et-button.encrypt");
+const buttonDecrypt = components(".et-button.decrypt");
+const buttonCopy = components(".et-copy");
 
 // Encrypt && Decript Text
 const keys = {
-    "a" : "ai",
-    "e" : "enter",
-    "i" : "imes",
-    "o" : "ober",
-    "u" : "ufat"
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
 };
 
-const encryptText = text => {
-    return text.split("").map(letter => {
-        if (keys[letter] !== undefined) return keys[letter];
-        else return letter;
-    }).join("");
-}
+const encryptText = (text) => {
+  return text
+    .split("")
+    .map((letter) => {
+      if (keys[letter] !== undefined) return keys[letter];
+      return letter;
+    })
+    .join("");
+};
 
-const decryptText = text => {
-    let decodeText = "";
-    for (let indexLetter = 0; indexLetter < text.length; indexLetter++) {
-        let letter = text[indexLetter];
-        if (keys[letter] !== undefined) {
-            let key = keys[letter];
-            let hidenText = text.slice(indexLetter, indexLetter + key.length);
-            if(hidenText === key){
-                indexLetter = indexLetter + (key.length - 1);
-            }
-        }
-        decodeText += letter;
+const decryptText = (text) => {
+  let decodeText = "";
+  for (let indexLetter = 0; indexLetter < text.length; indexLetter++) {
+    let letter = text[indexLetter];
+    if (keys[letter] !== undefined) {
+      let key = keys[letter];
+      let hidenText = text.slice(indexLetter, indexLetter + key.length);
+      if (hidenText === key) {
+        indexLetter = indexLetter + (key.length - 1);
+      }
     }
-    return decodeText;
+    decodeText += letter;
+  }
+  return decodeText;
 };
 
 const encrypt = () => {
-    const txt = typeText.value;
-    submittedText.innerText = encryptText(txt);
-    initialContent.style.display = "none";
-    submittedTextContainer.style.display = "flex";
-    submittedTextContainer.style.flexFlow = "column";
-    buttonCopy.style.display = "block";
-}
+  const txt = typeText.value;
+  showText.innerText = encryptText(txt);
+  notContent.style.display = "none";
+  content.style.display = "flex";
+};
 
 const decrypt = () => {
-    const txt = typeText.value;
-    submittedText.innerText = decryptText(txt);
-    initialContent.style.display = "none";
-    submittedTextContainer.style.display = "flex";
-    submittedTextContainer.style.flexFlow = "column";
-    buttonCopy.style.display = "block";
-}
+  const txt = typeText.value;
+  showText.innerText = decryptText(txt);
+  notContent.style.display = "none";
+  content.style.display = "flex";
+};
 
 // Copy submitted text
 const copy = () => {
-    const copyText = submittedText.value;
-    navigator.clipboard.writeText(copyText);
+  const copyText = showText.value;
+  navigator.clipboard.writeText(copyText);
 };
 
 // Avoid capital letters and special characters
-typeText.addEventListener("keydown", e => {
-    if (e.key.toUpperCase() === e.key && e.key !== " ") {
-        e.preventDefault();
-        alert("Solo se permiten letras minúsculas y sin acentos");
-    }
+typeText.addEventListener("keydown", (e) => {
+  if (e.key.toUpperCase() === e.key && e.key !== " ") {
+    e.preventDefault();
+    alert("Solo se permiten letras minúsculas y sin acentos");
+  }
 });
 
 // Adjust height of textareas
-typeText.addEventListener('input', () => {
-    typeText.style.height = 'auto';
-    typeText.style.height = `${typeText.scrollHeight}px`;
+typeText.addEventListener("input", () => {
+  typeText.style.height = "auto";
+  typeText.style.height = `${typeText.scrollHeight}px`;
 });
 
-submittedText.addEventListener('input', () => {
-    submittedText.style.height = 'auto';
-    submittedText.style.height = `${submittedText.scrollHeight}px`;
+showText.addEventListener("input", () => {
+  showText.style.height = "auto";
+  showText.style.height = `${showText.scrollHeight}px`;
 });
 
 // Remove text
